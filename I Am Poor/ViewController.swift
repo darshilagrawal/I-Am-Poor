6a3e7ca884790e1778adb61de2576d59abfd7d95
Darshil <61809014+darshilagrawal@users.noreply.github.com>
2021-01-26 14:13:59 +0530

    An App for Image and Text Display on IOS

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Darshil Agrawal on 23/03/20.
+//  Copyright © 2020 Darshil Agrawal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
